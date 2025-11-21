f56c65dad3495cd4f25f0083b07a157003628c1d
Fernando Lopez <frlopez88@icloud.com>
2025-11-20 20:19:13 -0600

    adding area and clothing problems

--- app.js
+++ app.js
@@ -9,3 +9,34 @@ function canVote(){
     }
 
 }
+
+function areaRectangle(){
+
+
+   let area = parseFloat(lengthRectangle.value) * parseFloat(widthRectangle.value);
+
+   //console.log(`The area is ${area}`)
+    result.textContent = `The area is ${area}`
+}
+
+function clothing(){
+
+    if(tempC.value >= 25){
+        //yes
+        
+        result.textContent= "Use light Clothing";
+    }else{
+        //no
+        if (tempC.value < 10){
+            //yes
+          
+           result.textContent= "Use warm Clothing";
+        }else{
+            //no
+            
+            result.textContent= "Use moderate Clothing";
+        }
+
+    }
+
+}
